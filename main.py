b86104a417d0d65a8a9286ae391df9b85e183d09
Longqian Huang <longqianh@gmail.com>
2020-07-26 14:32:44 +0800

    initialize with the current status

---
+++ main.py
@@ -0,0 +1,92 @@
+import cv2
+from pathlib import Path
+import torch
+import numpy as np
+from models.matching import Matching
+from models.utils import frame2tensor
+from models.utils import (compute_pose_error, compute_epipolar_error,
+                          estimate_pose,
+                          pose_auc, read_image,
+                          rotate_intrinsics, rotate_pose_inplane,
+                          scale_intrinsics)
+
+
+class Arg(object):
+
+  def __init__(self,
+               config={
+                   'superpoint': {
+                       'nms_radius': 4,
+                       'keypoint_threshold': 0.005,
+                       'max_keypoints': -1
+                   },
+                   'superglue': {
+                       # choices={'indoor', 'outdoor'}
+                       'weights': 'outdoor',
+                       'sinkhorn_iterations': 20,
+                       'match_threshold': 0.2,
+                   }
+               },
+               _input='assets/input_img/',
+               _output=None,
+               scene_mode='outdoor',
+               skip=1,
+               img_glob=['*.png', '*.jpg', '*.jpeg'],
+               max_length=1000000):
+
+    super(Arg, self).__init__()
+    # self.nms_radius=nms_radius
+    # self.keypoint_threshold=keypoint_threshold
+    self.input_dir = _input
+    # 'ID of a USB webcam, URL of an IP camera, '
+    # 'or path to an image directory or movie file'
+    self.config = config
+    self.output_dir = _output
+    if scene_mode == 'outdoor':
+      self.resize = [1600]
+      self.max_kpts = 2048
+      self.nms_radius = 3
+    else:
+      self.resize = [640]
+      self.max_kpts = 1024
+      self.nms_radius = 4
+
+    self.skip = skip
+    self.img_glob = img_glob
+    self.max_length = max_length
+
+  def set_config():
+    pass
+
+  def set_anchor_img():
+    pass
+
+
+def get_similarity(img1, img2, args, device='cpu'):
+  t_img0 = frame2tensor(img1, device)
+  t_img1 = frame2tensor(img2, device)
+
+  matching = Matching(args.config).eval().to(device)  # matching model
+  pred = matching({'image0': t_img0, 'image1': t_img1})
+  conf = pred['matching_scores0']
+  return torch.mean(conf).item(), torch.max(conf).item()
+
+
+# 下一步：set anchor以后想办法加速 把kpts直接存好 应该可以快一倍
+
+torch.set_grad_enabled(False)
+# device = 'cuda' if torch.cuda.is_available() and not opt.force_cpu else 'cpu'
+device = 'cpu'
+args = Arg(scene_mode='indoor')
+# 后续改进
+name0 = 'anchor.jpg'
+name1 = 'tomatch.jpg'
+# image0, inp0, scales0 = read_image(
+#     args.input_dir + name0, device, args.resize)
+# image1, inp1, scales1 = read_image(
+#     args.input_dir + name1, device, args.resize)
+# matches_path = 'assets/match_res.npz'
+# print(inp0)
+img0 = cv2.imread(args.input_dir + name0, cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread(args.input_dir + name1, cv2.IMREAD_GRAYSCALE)
+print(get_similarity(img0, img1, args))
---
+++ models/utils.py
@@ -0,0 +1,197 @@
+#  Origin:
+#  Magic Leap, Inc.
+#  Unpublished Copyright (c) 2020
+#  Originating Authors: Paul-Edouard Sarlin, Daniel DeTone, Tomasz Malisiewicz
+#  delete and fine tune: Peter_H
+
+from pathlib import Path
+import time
+from collections import OrderedDict
+from threading import Thread
+import numpy as np
+import cv2
+import torch
+
+
+# -- Image Processing --
+def process_resize(w, h, resize):
+    assert(len(resize) > 0 and len(resize) <= 2)
+    if len(resize) == 1 and resize[0] > -1:
+        scale = resize[0] / max(h, w)
+        w_new, h_new = int(round(w * scale)), int(round(h * scale))
+    elif len(resize) == 1 and resize[0] == -1:
+        w_new, h_new = w, h
+    else:  # len(resize) == 2:
+        w_new, h_new = resize[0], resize[1]
+
+    # Issue warning if resolution is too small or too large.
+    if max(w_new, h_new) < 160:
+        print('Warning: input resolution is very small, results may vary')
+    elif max(w_new, h_new) > 2000:
+        print('Warning: input resolution is very large, results may vary')
+
+    return w_new, h_new
+
+
+def frame2tensor(frame, device):
+    # print(torch.from_numpy(frame/255.).float())
+    # None 增加两个维度
+    # print(torch.from_numpy(frame/255.).float()[None,None].shape)
+    # torch.Size([1, 1, 480, 640])
+
+    # cv2.imshow('1',frame)
+    # cv2.waitKey(0)
+    return torch.from_numpy(frame / 255.).float()[None, None].to(device)
+
+
+def read_image(path, device, resize, rotation=0, resize_float=1):
+    image = cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        return None, None, None
+    w, h = image.shape[1], image.shape[0]
+    w_new, h_new = process_resize(w, h, resize)
+    scales = (float(w) / float(w_new), float(h) / float(h_new))
+
+    if resize_float:
+        image = cv2.resize(image.astype('float32'), (w_new, h_new))
+    else:
+        image = cv2.resize(image, (w_new, h_new)).astype('float32')
+
+    if rotation != 0:
+        image = np.rot90(image, k=rotation)
+        if rotation % 2:
+            scales = scales[::-1]
+
+    inp = frame2tensor(image, device)
+    return image, inp, scales
+
+
+# --- Pose Estimation ---
+
+
+def estimate_pose(kpts0, kpts1, K0, K1, thresh, conf=0.99999):
+    if len(kpts0) < 5:
+        return None
+
+    f_mean = np.mean([K0[0, 0], K1[1, 1], K0[0, 0], K1[1, 1]])
+    norm_thresh = thresh / f_mean
+
+    kpts0 = (kpts0 - K0[[0, 1], [2, 2]][None]) / K0[[0, 1], [0, 1]][None]
+    kpts1 = (kpts1 - K1[[0, 1], [2, 2]][None]) / K1[[0, 1], [0, 1]][None]
+
+    E, mask = cv2.findEssentialMat(
+        kpts0, kpts1, np.eye(3), threshold=norm_thresh, prob=conf,
+        method=cv2.RANSAC)
+
+    assert E is not None
+
+    best_num_inliers = 0
+    ret = None
+    # E: essential matrix
+    # param R Recovered relative rotation.
+    # param t Recovered relative translation.
+    for _E in np.split(E, len(E) / 3):
+        n, R, t, _ = cv2.recoverPose(
+            _E, kpts0, kpts1, np.eye(3), 1e9, mask=mask)
+        if n > best_num_inliers:
+            best_num_inliers = n
+            ret = (R, t[:, 0], mask.ravel() > 0)
+            # np.ravel: 将多维数组转换为一维
+    return ret
+
+
+def rotate_intrinsics(K, image_shape, rot):
+    """image_shape is the shape of the image after rotation"""
+    assert rot <= 3
+    h, w = image_shape[:2][::-1 if (rot % 2) else 1]
+    fx, fy, cx, cy = K[0, 0], K[1, 1], K[0, 2], K[1, 2]
+    rot = rot % 4
+    if rot == 1:
+        return np.array([[fy, 0., cy],
+                         [0., fx, w - 1 - cx],
+                         [0., 0., 1.]], dtype=K.dtype)
+    elif rot == 2:
+        return np.array([[fx, 0., w - 1 - cx],
+                         [0., fy, h - 1 - cy],
+                         [0., 0., 1.]], dtype=K.dtype)
+    else:  # if rot == 3:
+        return np.array([[fy, 0., h - 1 - cy],
+                         [0., fx, cx],
+                         [0., 0., 1.]], dtype=K.dtype)
+
+
+def rotate_pose_inplane(i_T_w, rot):
+    rotation_matrices = [
+        np.array([[np.cos(r), -np.sin(r), 0., 0.],
+                  [np.sin(r), np.cos(r), 0., 0.],
+                  [0., 0., 1., 0.],
+                  [0., 0., 0., 1.]], dtype=np.float32)
+        for r in [np.deg2rad(d) for d in (0, 270, 180, 90)]
+    ]
+    return np.dot(rotation_matrices[rot], i_T_w)
+
+
+def scale_intrinsics(K, scales):
+    scales = np.diag([1. / scales[0], 1. / scales[1], 1.])
+    return np.dot(scales, K)
+
+
+def to_homogeneous(points):
+    return np.concatenate([points, np.ones_like(points[:, :1])], axis=-1)
+
+
+def compute_epipolar_error(kpts0, kpts1, T_0to1, K0, K1):
+    kpts0 = (kpts0 - K0[[0, 1], [2, 2]][None]) / K0[[0, 1], [0, 1]][None]
+    kpts1 = (kpts1 - K1[[0, 1], [2, 2]][None]) / K1[[0, 1], [0, 1]][None]
+    kpts0 = to_homogeneous(kpts0)
+    kpts1 = to_homogeneous(kpts1)
+
+    t0, t1, t2 = T_0to1[:3, 3]
+    t_skew = np.array([
+        [0, -t2, t1],
+        [t2, 0, -t0],
+        [-t1, t0, 0]
+    ])
+    E = t_skew @ T_0to1[:3, :3]
+
+    Ep0 = kpts0 @ E.T  # N x 3
+    p1Ep0 = np.sum(kpts1 * Ep0, -1)  # N
+    Etp1 = kpts1 @ E  # N x 3
+    d = p1Ep0**2 * (1.0 / (Ep0[:, 0]**2 + Ep0[:, 1]**2)
+                    + 1.0 / (Etp1[:, 0]**2 + Etp1[:, 1]**2))
+    return d
+
+
+def angle_error_mat(R1, R2):
+    cos = (np.trace(np.dot(R1.T, R2)) - 1) / 2
+    cos = np.clip(cos, -1., 1.)  # numercial errors can make it out of bounds
+    return np.rad2deg(np.abs(np.arccos(cos)))
+
+
+def angle_error_vec(v1, v2):
+    n = np.linalg.norm(v1) * np.linalg.norm(v2)
+    return np.rad2deg(np.arccos(np.clip(np.dot(v1, v2) / n, -1.0, 1.0)))
+
+
+def compute_pose_error(T_0to1, R, t):
+    R_gt = T_0to1[:3, :3]
+    t_gt = T_0to1[:3, 3]
+    error_t = angle_error_vec(t, t_gt)
+    error_t = np.minimum(error_t, 180 - error_t)  # ambiguity of E estimation
+    error_R = angle_error_mat(R, R_gt)
+    return error_t, error_R
+
+
+def pose_auc(errors, thresholds):
+    sort_idx = np.argsort(errors)
+    errors = np.array(errors.copy())[sort_idx]
+    recall = (np.arange(len(errors)) + 1) / len(errors)
+    errors = np.r_[0., errors]  # 按行连接
+    recall = np.r_[0., recall]
+    aucs = []
+    for t in thresholds:
+        last_index = np.searchsorted(errors, t)
+        r = np.r_[recall[:last_index], recall[last_index - 1]]
+        e = np.r_[errors[:last_index], t]
+        aucs.append(np.trapz(r, x=e) / t)  # 角度误差限度内的准确率
+    return aucs
